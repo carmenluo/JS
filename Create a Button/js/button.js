c749528ee6d7e77ddc10a9570b81dcd95d7be1ad
Carmen Luo <sdluojm@gmail.com>
2019-07-30 15:02:59 -0400

    Day 8 - Create a button

---
+++ Create a Button/js/button.js
@@ -0,0 +1,10 @@
+/*When I use CreateElement i could not apply css to the button because it loads css file first*/
+  var myButton=document.getElementById('btn');
+
+var num=0;
+
+myButton.addEventListener('click',e=>{
+    num+=1;
+    myButton.innerHTML=num;
+});
+document.body.appendChild(myButton);
